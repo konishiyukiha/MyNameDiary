e1be2aa0ef86564404a5550c7154806ebb426dea
konishiyukiha <konisdm.mm.93@gmail.com>
2017-03-08 20:19:47 +0000

    first commit

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,13 +1,44 @@
 class ArticlesController < ApplicationController
-	def new
-	end
+	before_action :set_articles,only:[:show,:edit,:update,:destroy]
+		# @article = Article.new (form_for使うとき)
 
 	def create
-		@articles = Article.new
-		@articles.title = params[:title]
-		@articles.body = params[:body]
-		@articles.author = params[:author]
-		@articles.save
-		redirect_to'/articles/new'
+		@article = Article.new(article_params)
+		@article.save
+		redirect_to show_article_path(@article.id)
 	end
+
+	def index
+		@articles = Article.all
+	end
+
+	def show
+	end
+
+	def edit
+
+	end
+
+	def update
+		@article.title = params[:title]
+		@article.body = params[:body]
+		@article.author = params[:author]
+		@article.save
+		redirect_to show_article_path(@article.id)
+	end
+
+	def destroy
+		@article.destroy
+		redirect_to articles_index_path
+	end
+
+	private
+	def set_articles
+		@article = Article.find(params[:id])
+	end
+
+	def article_params
+		params.require(:article).permit(:title,:body,:author)
+	end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,12 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get"/top"=>"home#top"
   get"/articles/new"=>"articles#new"
+  #左と右が同じときは省略可（get'articles/new'のみ）
   post '/articles/new' => 'articles#create'
-end
+  get'/articles/index'=>'articles#index'
+  get'/articles/:id' =>'articles#show', as:'show_article'
+  get'/articles/:id/edit'=>'articles#edit', as:'edit_article'
+  patch '/articles/:id'=> 'articles#update', as:'update_article'
+  delete '/articles/:id'=> 'articles#destroy', as:'destroy_article'
 
-#homecontrollerを行うと、homecontrollerの中のtopという処理を行う
-#TOPという処理はviewでいうhomeの中のtop.html.erbを表示するということ
+end
